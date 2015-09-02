b0ac3039be548b652afbf6897c3c36dd399ae8ff
Sergio Alejandro Valencia López <alejandrov.net@gmail.com>
2015-09-02 12:28:40 -0500

    Project MVC PHP native
    Example query on Database

---
+++ project/controller/SiteController.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Class SiteController.php
+ */
+class SiteController extends Controller
+{
+
+    /**
+     * Constructor de la clase
+     *
+     * @author  Sergio Alejandro Valencia López <alejandrov.net@gmail.com>
+     * @version 1
+     * @copyright alejandrov.net
+     * @return \SiteController
+    @access public
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $arrResult = [];
+        $bolQuery = false;
+        if (isset($_POST['txtDocument'])) {
+            $bolQuery = true;
+            $objConection = new Conection();
+
+            $sql = 'SELECT  usu_id
+                            ,usu_nombres
+                            ,usu_apellidos
+                            ,usu_email
+                    FROM usuario
+                    WHERE usu_id =:usu_id';
+            error_reporting(E_ERROR | E_WARNING | E_PARSE);
+            $objConection->query($sql);
+            $objConection->bind(':usu_id', $_POST['txtDocument']);
+            $arrResult = (array)$objConection->single();
+
+            if (isset($arrResult[0]) && (string)$arrResult[0] === '') {
+                $arrResult = [];
+            }
+        }
+
+        $this->render(
+            'example.php', ['data' => $arrResult
+                , 'query' => $bolQuery]
+        );
+    }
+
+}
